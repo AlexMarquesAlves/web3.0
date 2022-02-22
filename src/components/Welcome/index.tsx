0f3f4168119ba9d85cdd6d1c1bf17d0aa09fa6cc
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-02-22 16:32:44 -0300

    Create Welcome Component

---
+++ src/components/Welcome/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+interface WelcomeProps {
+  children: ReactNode;
+}
+
+function Welcome({ children }: WelcomeProps) {
+  return (
+    <>
+      <h1>Welcome</h1>
+      {children}
+    </>
+  );
+}
+
+export default Welcome;
