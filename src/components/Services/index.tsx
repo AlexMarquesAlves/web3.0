8098c9178666b7c020e91c68ab7bbab936426245
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-02-22 16:30:50 -0300

    Create Services Component

---
+++ src/components/Services/index.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from 'react';
+
+interface ServicesProps {
+  children: ReactNode;
+}
+
+function Services({ children }: ServicesProps) {
+  return (
+    <>
+      <h1>Services</h1>
+      {children}
+    </>
+  );
+}
+
+export default Services;
